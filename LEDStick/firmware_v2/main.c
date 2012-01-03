963493582eaf5ae9960d4ef42fc9e20e45c26c0c
Seb <github@seb.exse.net>
2012-01-03 03:24:18 +0100

    changes

--- LEDStick/firmware_v2/main.c
+++ LEDStick/firmware_v2/main.c
@@ -19,7 +19,36 @@
 // SOUT == MISO/PB4
 // GSCLK == PB0
 
+volatile uint8_t activation = 0;
+
 ISR(PCINT2_vect)
+{
+	if(timeout != 0)
+	{
+		return;
+	};
+	timeout = 2;
+	
+	if(activation == 0)
+	{
+		activation = 1;
+	}
+	else
+	{
+		SetLed(0,0,0,0);
+		SMCR |= (1<<SM1)|(1<<SE);
+		
+		// in the end we like to reenable pcie2 also, to have the second button free for mode-change
+		
+		PCICR &= ~(1<<PCIE2);
+		asm volatile("sei");
+		asm volatile("sleep");
+		wdt_enable(WDTO_250MS);
+		while(1);
+	}
+}
+
+ISR(INT1_vect)
 {
 	wdt_enable(WDTO_250MS);
 }
@@ -28,6 +57,10 @@ int main (void)
 {
 	cli();
 	wdt_reset();
+	if( (MCUSR & (1<<WDRF))==(1<<WDRF))
+	{
+		activation = 1;
+	}
 	MCUSR &= ~(1<<WDRF);
 	WDTCSR |= (1<<WDCE) | (1<<WDE);
 	WDTCSR = 0x00;
@@ -38,10 +71,16 @@ int main (void)
 	ADC_Init();
 	TIMER1_Init();
 	
-	PORTD |= (1<<PORTD3);
-	PCMSK2 |= (1<<PCINT19);
+	PORTD |= (1<<PORTD4);
+	PCMSK2 |= (1<<PCINT20);
 	PCICR |= (1<<PCIE2);
 	
+	PORTD |= (1<<PORTD3);
+//	EICRA |= (1<<ISC11);
+	EIMSK |= (1<<INT1);
+	
+	
+	PRR|=(1<<PRTWI)|(1<<PRTIM2)|(1<<PRTIM0)|(1<<PRUSART0);
 
 	sei ();
 	
@@ -49,13 +88,22 @@ int main (void)
 	
 	SetLed(0,0,0,0);
 
+
 	initialized = 1;
-
-	SetLed(0,0,10,0);
-//	timeout = 2;
-//	while(timeout);
+	
 
 
+	while(activation == 0)
+	{
+		SetLed(1,0x80,0x40,0x40);
+		SetLed(2,0x08,0x08,0x08);
+		SetLed(3,0,0,10);
+		SetLed(4,10,0,0);
+		SetLed(5,0,10,0);
+	}
+	
+	
+	
 
 	while(1)
 	{
